f7a6e52572422a69ddd610ff671499cb85d1ede8
Bbianka-coder <79424150+Bbianka-coder@users.noreply.github.com>
2021-04-26 21:11:59 +0200

    Add files via upload

---
+++ H22TQ1_0412/H22RQ1_alarm.c
@@ -0,0 +1,38 @@
+//
+//  main.c
+//  
+//
+//  Created by Balogh Bianka on 2021. 04. 12..
+//
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <signal.h>
+#define SECOND 1
+
+void do_nothing();
+void do_int();
+
+int main ()
+{
+    int i;
+    unsigned sec=1;
+
+    signal(SIGINT, do_int);
+
+for (i=1;i<8;i++) {
+    alarm(sec);
+    signal(SIGALRM, do_nothing);
+    printf("  %d varok de meddig?\n",i);
+    pause();
+    }
+}
+void do_nothing(){ ;}
+
+void do_int() {
+    printf(" Int jott ");
+    signal(SIGINT,SIG_IGN);
+}
+
+
